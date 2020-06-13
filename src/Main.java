38454c43c253d7ca3eb89488b5883425f7223fec
박 준영 <wnsdud1302@naver.com>
2020-06-13 14:00:48 +0000

    chp6 리턴 값

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 import chp6.chp6;
 public class Main{
    public static void main(String[] args) throws Exception{
+       int a[] = {1,2,3,4,5};
        chp6 c6 = new chp6();
-       c6.add();
+       int sum1 = c6.sum1(a);
+       System.out.println(sum1);
+       int sum2 = c6.sum2(1,2,3,4,5);
+       System.out.println(sum2);
+       
        
 
     }
--- src/chp6/chp6.java
+++ src/chp6/chp6.java
@@ -1,7 +1,22 @@
 package chp6;
 
 public class chp6 {
-    public int add(){
-        return 0;
+    public chp6(){
+        System.out.println();
+        System.out.println("클래스를 생성합니다.");
+    }
+    public int sum1(int value[]){
+        int sum = 0;
+        for(int i = 0; i < value.length; i++){
+            sum += i;
+        }
+        return sum;
+    }
+    public int sum2(int ... value){
+        int sum = 0;
+        for(int i = 0; i < value.length; i++){
+            sum += i;
+        }
+        return sum;
     }
 }
